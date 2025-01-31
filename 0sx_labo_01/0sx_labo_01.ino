3c30e1ad832000b54195ec63554c533b9be8b0e8
S-Beaulieu <Beaulieu-Salomon@protonmail.com>
2025-01-31 11:55:03 -0500

    Utilisation de Fonction pour chaque État et Amélioration de la lisibilité du code WIP

--- 0sx_labo_01/0sx_labo_01.ino
+++ 0sx_labo_01/0sx_labo_01.ino
@@ -1,16 +1,16 @@
 
 const short NUMBER_OF_BLINK = 4;
 const short BLINK_INTERVAL = 500;  
-const short TWO_SECOND  = 2000; 
+const short TWO_SECOND  = 2000;
+const short FADE_TOTAL_TIME = 2048;
 
-int ledState = LOW;  // ledState état de la lumiére
-unsigned short  appState = 1; //État de l'application
-unsigned short  currentBlink = 0; //État de l'application
-short fadeAmout = 4;
-short brightness = 255;
-unsigned long currentMilli=0;
-unsigned long previousMillis = 0;  // dernier changement de la led
-unsigned short stateOfcycle = 0;
+unsigned short ledState = LOW;
+unsigned short  appState = 1; 
+
+
+//Gestion du temps 
+unsigned long currentMillis=0;
+unsigned long previousMillis = 0;
 
 void setup() {
   // set the digital pin as output:
@@ -20,77 +20,112 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
+  //Sinon usigned long currentMillis = ....
+  currentMillis = millis();
   
 
   switch(appState){
-
     case 1:
-      //Allume la LED
-      if (currentMillis-previousMillis >= TWO_SECOND) {
-          previousMillis = currentMillis;
-          ledState!=ledState;
-          appState++;
-        }
-      Serial.println("Allume – 1993855");
+      //Allumer la LED
+      turnTheLEDOnOrOff(TWO_SECOND, HIGH);
       break;
     case 2:
       //Clignotement de la LED
-      if(currentBlink<=NUMBER_OF_BLINK){
-        if(currentMillis-previousMillis >= BLINK_INTERVAL) {
-          previousMillis = currentMillis;
-          if (ledState == LOW) {
-              ledState = HIGH;
-              currentBlink++;
-              Serial.println("Clignotement – 1993855");
+      blinkTheLED(NUMBER_OF_BLINK,BLINK_INTERVAL);
+      break;
+    case 3: 
+      //Fondu de la lumiére LED 
+      fadeTheLED(FADE_TOTAL_TIME,255);
+      break;
+    case 4:
+      //Éteindre la Led
+      turnTheLEDOnOrOff(TWO_SECOND, LOW);
+      break;
+  }
+}
+void fadeTheLED(const int TOTAL_DURATION, const short START_FADE_VALUE){
+  static short brightness = START_FADE_VALUE;
+  static short fadeAmout = (brightness>=127) ? -4 : 4;
 
-
-          } else {
-              ledState = LOW;
-          }
-          digitalWrite(LED_BUILTIN, ledState);
-        }
-      }else{
-        currentBlink = 0;
-        digitalWrite(LED_BUILTIN, HIGH);
-        ledState=255;
-        ++appState;
-      }
-        break;
-      case 3: 
-        //Fondu de la lumiére LED 
-        Serial.println("Variation – 1993855 ");
-        if(currentMillis-previousMillis >= 8) {
-          analogWrite(LED_BUILTIN, ledState);
-          previousMillis = currentMillis;
-          ledState -= fadeAmout;
-          if (ledState <=0) {
-            Serial.println("Variation – 1993855 ");
-            ledState=LOW;
-            digitalWrite(LED_BUILTIN, ledState);
-            ++appState;
-            previousMillis = currentMillis;
-          }
-        }
-        break;
-      case 4:
-        //Éteint la lumiére  LED
-        Serial.println("Éteint – 1993855");
-        if (currentMillis-previousMillis >= TWO_SECOND) {
-          previousMillis = currentMillis;
-          ledState =HIGH;
-          digitalWrite(LED_BUILTIN, ledState);
-          appState=1;
-        }
-        break;
+  ledState = (brightness>=127) ?  HIGH: LOW;
+  //Calculer le temps pour chaque étape
+  static int durationFadeStep = (TOTAL_DURATION/(brightness/fadeAmout));
+  //Mettre le temps de la duration positif
+  durationFadeStep = (durationFadeStep < 0) ? (durationFadeStep*-1): durationFadeStep;
+  static int showDA = 0;
+  
+  //On afficher l'état et le DA
+  if(showDA<1){
+    Serial.println("\nVariation – 1993855 ");
+    digitalWrite(LED_BUILTIN, ledState);
+    ++showDA;
+  }
+  if(currentMillis-previousMillis >= durationFadeStep) {
+    analogWrite(LED_BUILTIN, brightness);
+    previousMillis = currentMillis;
+    brightness += fadeAmout;
+    if(brightness<=0){
+      appState++;
+      brightness = START_FADE_VALUE;
+      showDA=0;
     }
-    
+  }
+}
+void  blinkTheLED(const int MAX_BLINK, const int BLINK_INTERVAL){
+  static int compteur = 0;
+  static unsigned short currentBlink = 0; //BLink en cours
 
+  //On afficher l'état et le DA
+  if(compteur<1){
+    Serial.print("Clignotement - 1993855 --> ");
+    compteur++;
+  }
+
+  //On s'assure que le nombre de clignotement effectuer ne dépasse pas le nombre Maximum
+  if(currentBlink<MAX_BLINK){
+    if(currentMillis-previousMillis >= BLINK_INTERVAL) {
+      previousMillis = currentMillis;
+      //Changer l'état de la led à l'aide d'un TRUE | FALSE 
+      ledState = (ledState == LOW) ? HIGH : LOW;
+      digitalWrite(LED_BUILTIN, ledState);
+      if (ledState) {
+        currentBlink++;
+        Serial.print(compteur++);
+        Serial.print(" ");
+      }
+    }
+  }else{
+    //Rénitialisation pour la prochaine loop()
+    compteur=0;
+    currentBlink=0;
+    appState++;
+  }
   
 }
+void turnTheLEDOnOrOff(const int TIME_TO_WAIT, const int VALUE){
+  static int showDA = 0;
+  ledState = (VALUE==1) ? HIGH : LOW;
+  if(showDA<1){
+    if(!ledState){
+    Serial.println("Éteint - 1993855");
+    digitalWrite(LED_BUILTIN, ledState);
+    }else{
+    Serial.println("Allumé - 1993855");
+    digitalWrite(LED_BUILTIN, ledState);
+    }
+    showDA++;
+  }
+  if (currentMillis-previousMillis >= TIME_TO_WAIT) {
+  showDA=0;
+  previousMillis = currentMillis;
+  if(!ledState){
+    appState=1;
+    digitalWrite(LED_BUILTIN, ledState);
+  }else{
+    appState++;
+  }
 
-void switchTheAppState(){
-  
+  }
 }
 
 
